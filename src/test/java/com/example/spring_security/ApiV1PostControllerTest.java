6ad36d78d4a8c0020ba1800c83cb2ae8c714fc7c
jxxngin <wwd0203@daum.net>
2025-02-18 15:09:57 +0900

    [feat] user detail service

--- src/test/java/com/example/spring_security/ApiV1PostControllerTest.java
+++ src/test/java/com/example/spring_security/ApiV1PostControllerTest.java
@@ -288,7 +288,7 @@ public class ApiV1PostControllerTest {
 
     @Test
     @DisplayName("글 작성")
-    @WithUserDetails("user")
+    @WithUserDetails("user1")
     void write1() throws Exception {
         String apiKey = "user1";
         String title = "새로운 글 제목";
