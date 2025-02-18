ac4f91d8c3b12dd9142d4da3c3c91c53c983803e
jxxngin <wwd0203@daum.net>
2025-02-18 15:30:36 +0900

    [feat] custom authenticate

--- src/main/java/com/example/spring_security/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/example/spring_security/global/security/CustomAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.example.spring_security.global.security;
 
+import com.example.spring_security.domain.member.member.entity.Member;
+import com.example.spring_security.domain.member.member.service.MemberService;
 import com.example.spring_security.global.Rq;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -10,12 +12,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class CustomAuthenticationFilter extends OncePerRequestFilter {
 
     private final Rq rq;
+    private final MemberService memberService;
 
     @Override
     protected void doFilterInternal(
@@ -23,7 +27,30 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain
     ) throws ServletException, IOException {
-        rq.setLogin("user1");
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (!authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String apiKey = authorizationHeader.substring("Bearer ".length());
+        Optional<Member> opMember = memberService.findByApiKey(apiKey);
+
+        if (opMember.isEmpty()) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        Member actor = opMember.get();
+
+        rq.setLogin(actor.getUsername());
+
         filterChain.doFilter(request, response);
     }
 
